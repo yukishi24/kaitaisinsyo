0df68802354fa96845adb68d81e9a1d50be75d30
yukishi24 <yukishi24github@gmail.com>
2023-05-14 16:24:37 +0900

    crate: response logic

--- app/src/main/java/com/example/demo/hello/HelloController.java
+++ app/src/main/java/com/example/demo/hello/HelloController.java
@@ -1,7 +1,10 @@
 package com.example.demo.hello;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * hello用のコントローラー
@@ -22,5 +25,24 @@ public class HelloController {
 	@GetMapping("/hello")
 	public String getHello() {
 		return "hello";
+	}
+	
+	/**
+	 * hello.htmlから受け取った文字列をModelに追加してresponse.htmlに遷移する。
+	 * <br>
+	 * @PostMapping("/hello")httpからのpostリクエストを受け取るアノテーション。<br>
+	 * @RequestParam("text1")String str	画面で入力されたパラメータを受け取るアノテーション
+	 * @param str	hello.htmlのformから受け取る文字列。
+	 * @param model	response.htmlに送るためのmodel
+	 * @return		/hello/response.html
+	 */
+	@PostMapping("/hello")
+	public String postRequest(@RequestParam("text1")String str, Model model) {
+		
+		// 画面から受け取った文字列をModelに追加する。
+		model.addAttribute("sample",str);
+		
+		// response.htmlに遷移する。
+		return "/hello/response";
 	}
 }
