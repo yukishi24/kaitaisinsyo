abb6ccfd95c4876d8ba90a0aa976abdf4949b6d4
yukishi24 <yukishi24github@gmail.com>
2023-05-29 20:50:10 +0900

    add: validtion logic

--- app/src/main/java/com/example/demo/form/SignupForm.java
+++ app/src/main/java/com/example/demo/form/SignupForm.java
@@ -2,7 +2,15 @@ package com.example.demo.form;
 
 import java.util.Date;
 
+import org.hibernate.validator.constraints.Length;
 import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 /**
  * 画面の入力内容とマッピングするレコードクラス
@@ -11,12 +19,26 @@ import org.springframework.format.annotation.DateTimeFormat;
  *
  */
 public record SignupForm(
+		@NotBlank
+		@Email
 		String userId,
+		
+		@NotBlank
+		@Length(min = 4,max = 100)
+		@Pattern(regexp = "^[a-zA-Z0-9]+$")
 		String password,
+		
+		@NotBlank
 		String userName,
+		
 		@DateTimeFormat(pattern = "yyyy/mm/dd")
+		@NotNull
 		Date birthday,
+		
+		@Min(20) @Max(100)
 		Integer age,
+		
+		@NotNull
 		Integer gender
 		) {
 
